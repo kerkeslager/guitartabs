6095962c5a18a8d775d24f04c468dd9652344edd
David Kerkeslager <kerkeslager@gmail.com>
2018-03-23 10:13:51 -0400

    Add the ability to edit your own tabs

--- tabs/views.py
+++ tabs/views.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import PermissionDenied
+from django.forms.models import model_to_dict
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -20,6 +22,31 @@ def create(request):
 
     if request.method == 'GET':
         tab_form = forms.TabForm()
+        context = { 'tab_form': tab_form }
+        return render(request, 'tabs/create.html', context)
+
+    if request.method == 'POST':
+        tab = models.Tab(
+            user=request.user,
+            name=request.POST['name'],
+            body=request.POST['body'],
+        )
+        tab.save()
+
+        return HttpResponseRedirect(reverse('tabs:detail', args=(tab.pk,)))
+
+def edit(request, tab_id):
+    tab = get_object_or_404(models.Tab, pk=tab_id)
+
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('users:login') + '?next=' + reverse('tabs:edit', tab_id=tab.pk))
+
+    if not request.user == tab.user:
+        raise PermissionDenied
+
+
+    if request.method == 'GET':
+        tab_form = forms.TabForm(initial=model_to_dict(tab))
         context = { 'tab_form': tab_form }
         return render(request, 'tabs/create.html', context)
 
