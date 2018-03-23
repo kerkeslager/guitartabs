1b44526fcc1641231a52959cdc92f9c573c1899e
David Kerkeslager <kerkeslager@gmail.com>
2018-03-22 23:57:04 -0400

    Add 404 handler and links to tabs

--- tabs/views.py
+++ tabs/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from . import models
 
@@ -9,6 +9,6 @@ def index(request):
     return render(request, 'tabs/index.html', context)
 
 def detail(request, tab_id):
-    tab = models.Tab.objects.get(pk=tab_id)
+    tab = get_object_or_404(models.Tab, pk=tab_id)
     context = { 'tab': tab }
     return render(request, 'tabs/detail.html', context)
